3bada984e61121bc34046fa07fcdee8a09802ce0
Roland <askelof.roland@gmail.com>
2020-05-27 09:15:36 +0200

    Vue.js init

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
-<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+Vue.component('product', {
+
+  props:{
+
+  },
+
+  template: `
+  <div class="producto">
+    <h1>{{title}}</h1>
+    <ul>
+      <li v-for="detail in details"> {{detail}}</li>
+    </ul>
+    <button v-on:click="addToCart">Add to Cart</button>
+    <p>Cart: {{cart}}</p>
+    <button v-on:click="removeFromCart">Remove from Cart</button>
+
+  </div>
+  `,
+  data(){
+    return{
+      brand: 'Superfly',
+      product: 'Socks',
+      details: ["1","2","3","4","5"],
+      cart : 1,
+    }
+  },
+
+  methods: {
+     addToCart: function(){
+       this.cart++;
+     },
+     removeFromCart: function(){
+       this.cart--;
+     }
+   },
+   computed: {
+     title() {
+       return this.brand + ' ' + this.product
+     }
+   }
+
+
+ })
+
+
+var app = new Vue({
+  el: '#app'
+
+  })
